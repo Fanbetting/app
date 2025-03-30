3b478ae37507ce3f9971307860f1d7659a277142
Simon Samuel <salviensky@gmail.com>
2025-03-30 09:41:39 +0100

    feat: pushed latest ABI and Clients feat: updated the app theme chore: fixed UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,11 @@ export default function HomePage() {
       <AccountInfo />
 
       <Tabs defaultValue="tickets" className="space-y-4">
-        <TabsList className="flex justify-between">
-          <div>
-            <TabsTrigger value="tickets">Tickets</TabsTrigger>
-            <TabsTrigger value="transactions">Recent Transactions</TabsTrigger>
-          </div>
-
-          <CountdownTimer />
+        <TabsList className="flex w-fit">
+          <TabsTrigger value="tickets">
+            <CountdownTimer />
+          </TabsTrigger>
         </TabsList>
-
-        <div></div>
 
         <TabsContent value="tickets" className="space-y-8">
           <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-3">
@@ -32,10 +27,6 @@ export default function HomePage() {
             </div>
             <TicketEarnings />
           </div>
-        </TabsContent>
-
-        <TabsContent value="transactions" className="space-y-4">
-          <div className="grid h-full gap-4 md:grid-cols-1 lg:grid-cols-3"></div>
         </TabsContent>
       </Tabs>
     </main>
--- src/components/shared/app-header.tsx
+++ src/components/shared/app-header.tsx
@@ -8,11 +8,13 @@ import WinningTicket from "./winning-ticket";
 export default function Header() {
   return (
     <header className="flex w-full items-center justify-between px-8 py-8">
-      <Link href="/">
-        <FanbetLogo width="64" height="64" />
-      </Link>
+      <div className="flex flex-row items-center gap-8">
+        <Link href="/">
+          <FanbetLogo width="64" height="64" />
+        </Link>
 
-      <WinningTicket />
+        <WinningTicket />
+      </div>
       <div className="flex flex-row justify-between gap-4">
         <GameStatus />
         <ConnectButton />
